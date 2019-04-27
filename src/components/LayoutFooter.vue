28867715cc710a71d47ea22dd4ed8268735ba8df
soongil kim <alasheep@gmail.com>
2019-04-28 07:12:05 +0900

    update README.md

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <hr>
-        <p>Made in suwon, Korea. This website exists thanks to the open source projects <a href="https://vuejs.org/">VueJS</a>, <a href="https://gridsome.org">Gridsome</a>, <a href="https://bulma.io/">Bulma</a> & <a href="https://code.visualstudio.com/">Code</a>.</p>
+        <p>Made in suwon, Korea. Thanks to the open source projects <a href="https://vuejs.org/">VueJS</a>, <a href="https://gridsome.org">Gridsome</a>, <a href="https://bulma.io/">Bulma</a> & <a href="https://github.com/danvega/danvega-dev">Dan Vega</a>.</p>
     </footer>
 </template>
 

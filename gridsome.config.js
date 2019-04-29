af202494e1e3796037589bffcc5c976e145830c6
soongil kim <alasheep@gmail.com>
2019-04-29 20:23:55 +0900

    adjust my google analystic id

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,7 +8,7 @@ module.exports = {
   siteName: "Dan Vega",
   siteUrl: "https://www.danvega.dev",
   siteDescription: "Person blog of Dan Vega",
-  icon: "src/img/a-favicon.png",
+  icon: "src/img/alasheep-favicon.png",
   plugins: [{
       use: '@gridsome/source-filesystem',
       options: {

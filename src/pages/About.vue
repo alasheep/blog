6028a287e6341a86fe61ee6400fec5c20e2af347
alasheep <alasheep@gmail.com>
2020-07-03 13:31:36 +0900

    update About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,15 +3,13 @@
     <div class="content">
       <div class="columns is-centered">
         <div class="column is-two-thirds">
-          <h1 class="title is-2">Hi, I’m Soongil Kim.</h1>
-          <p><h1 class="title is-2">你好, 我叫金淳吉.</h1></p>
-          <br>
-          <p>안녕하세요. 김순길 입니다.</p>
-          <p>40대 개발자로, 초심으로 돌아가기 위해 블로그 개설했습니다.</p>
-          <p>Embedded SW, Mobile App(Android/iOS) 개발했었고,</p>
-          <p>현재는 Backend(Spring, SpringBoot) 서버를 개발하고 있습니다.</p>
-          <p>관심사는 판타지 소설, 프로그래밍, 여행, 중국 문화 및 중국어 입니다.</p>
-          <p>한달에 1번은 글을 올리도록 노력하겠습니다 ^.^</p>
+          <h1 class="title is-2">Have a nice day.!!!</h1>
+          <p></p>
+          <p>안녕하세요. 40대 개발자 입니다..</p>
+          <p>Embedded SW(DTV Device Driver), Mobile App(Android/iOS) 개발했었고,</p>
+          <p>현재는 Backend(SpringBoot) 서버를 개발하고 있습니다.</p>
+          <p>좋아하는건 책읽기, 여행, 가족 입니다.</p>
+          <p>자주 글 올리도록 노력하겠습니다. ^.^</p>
         </div>
         <div class="column right-side">
           <g-image src="../assets/img/andy-avatar.png" class="avatar" alt="Dan Vega Avatar"/>
